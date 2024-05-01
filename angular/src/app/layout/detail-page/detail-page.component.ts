67efbaba86a7c15d278415bfe0df3c34f150a6db
mdos-san <mdossan@protonmail.com>
2024-05-01 09:00:55 +0200

    angular(layout): Layout de page: Header, summary, detail, actions

---
+++ angular/src/app/layout/detail-page/detail-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "detail-page",
+  templateUrl: "./detail-page.component.html",
+  styleUrl: "./detail-page.component.css",
+})
+export class DetailPageComponent {}
